7edf96f5938ec73c6093f249e54f14719d9e4884
Munazza Sultana <sultanamunazza3@gmail.com>
2026-01-09 18:58:08 +0530

    Created folders & added transcribe

---
+++ Backend/Server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+
+app.post("/ask", async (req, res) => {
+  const response = await axios.post("http://localhost:8000/chat", {
+    question: req.body.question
+  });
+  res.json(response.data);
+});
+
+app.listen(3000, () => console.log("Node backend running"));
