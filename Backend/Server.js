42f3c61773219691feee88b0073b58c832be5397
Munazza Sultana <sultanamunazza3@gmail.com>
2026-01-25 19:52:01 +0530

    added transcript to frontend

--- Backend/Server.js
+++ Backend/Server.js
@@ -7,30 +7,30 @@ const axios = require("axios");
 app.use(express.urlencoded({extended : true}));
 app.use(express.json({ limit : "10mb" }));
 
-console.log("STATIC DIR:", path.join(__dirname, "../Frontend"));
+// console.log("STATIC DIR:", path.join(__dirname, "../Frontend"));
 
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
-app.post("/ask", async (req, res) => {
+app.post("/transcribe", async (req, res) => {
   try {
     const response = await axios.post(
-      "http://localhost:8000/chat",
-      { question: req.body.question }
+      "http://localhost:8000/transcribe",
+      { videoLink: req.body.videoLink }
     );
 
     res.json(response.data);
   } catch (err) {
-    res.status(500).json({ error: "Python service not reachable" });
+    res.status(500).json({ error: err.message });
   }
 });
 
-<<<<<<< HEAD
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../Frontend', 'home.html'));
 })
+app.get('/summary', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend', 'summary.html'));
+})
 
-=======
->>>>>>> 911955afb57c530aa546c37359eefc48c6f47016
 app.listen(3000, () => {
   console.log("✅ Node backend running at http://localhost:3000");
 });
